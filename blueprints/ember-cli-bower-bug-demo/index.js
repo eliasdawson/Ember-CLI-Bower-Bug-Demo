69b3108cfda6d69e80edaf8c95b5fd93b0e4f967
Elias Dawson <elias.dawson@crowdstrike.com>
2017-02-01 16:24:59 -0600

    Added addon name workaround to address install error.

--- blueprints/ember-cli-bower-bug-demo/index.js
+++ blueprints/ember-cli-bower-bug-demo/index.js
@@ -5,6 +5,12 @@ const RSVP = require('rsvp');
 module.exports = {
   description: 'Sample addon to demonstrate reported ember-cli bug',
 
+  normalizeEntityName: function() {
+    // allows us to run ember -g ember-cli-bootswatch and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  },
+
   afterInstall: function(options) {
     return RSVP.all([
       this.addBowerPackageToProject('lodash'),
