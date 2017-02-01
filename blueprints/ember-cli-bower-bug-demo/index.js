60d3258ebda521768ed67501b870a65b68d2b0cb
Elias Dawson <elias.dawson@crowdstrike.com>
2017-02-01 16:15:56 -0600

    Added missed import.

--- blueprints/ember-cli-bower-bug-demo/index.js
+++ blueprints/ember-cli-bower-bug-demo/index.js
@@ -1,9 +1,12 @@
 /*jshint node:true*/
+
+import Ember from 'ember';
+
 module.exports = {
   description: 'Sample addon to demonstrate reported ember-cli bug',
 
   afterInstall: function(options) {
-    return RSVP.all([
+    return Ember.RSVP.all([
       this.addBowerPackageToProject('lodash'),
       this.addBowerPackageToProject('moment')
     ]);
